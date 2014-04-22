baa5d82b096b280341589e470fc30c3216b059ae
Roger D. Peng [amelia] <rdpeng@gmail.com>
2014-04-22 10:09:22 -0400

    Updated code for Peer Assessment project
    This commit updates the code to complete the peer assessment project.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
+# Jon Rogers (GitHub ID is VanFalk)
+# Peer Assessment Project
 
-## Write a short comment describing this function
+# The functions in this code utilize "solve" and the "<<-" operator to compute
+# and return the inverse of a square matrix. The code is nearly identical to the
+# examples provided in the documentation as the process is the same with a
+# swapping of fucntions
 
-makeCacheMatrix <- function(x = matrix()) {
 
+# The "makeCacheMatrix" function creates a special "matrix" object that can cache 
+# its inverse by utilizing the "<<-" operator 
+
+makeCacheMatrix <- function(x = numeric()) {
+	s <- NULL
+	set <- function(y) {
+		     x <<- y
+		     s <<- NULL
+	}
+  get <- function() x
+	setsolve <- function(solve) s <<- solve
+	getsolve <- function() s
+	list(set = set, get = get,
+			 setsolve = setsolve,
+			 getsolve = getsolve)
 }
 
-
-## Write a short comment describing this function
+# The "cacheSolve" function computes the inverse of the special "matrix" 
+# returned by makeCacheMatrix above. If the inverse has already been calculated
+# (and the matrix has not changed), then cachesolve will retrieve the 
+# inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+	       s <- x$getsolve()
+	       if(!is.null(s)) {
+		       message("returning cached data")
+		       return(s)
+	 }
+	 data <- x$get()
+	 s <- solve(data, ...)
+	 x$setsolve(s)
+	 s
 }
